570859f21bf7640f80846dbf867c6459bb237cd6
DanielleInkster <danielle.inkster@gmail.com>
2020-05-06 17:41:17 -0600

    SeriesList displays

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
 const SeriesList = (props) =>{
     return(
-        <div> SeriesList div</div>
+        <div> {props.list.map(series =>
+            <li>
+                <img src={series.show.image.medium} />
+                <br/>
+                {series.show.name}
+                <br />
+                {ReactHtmlParser(series.show.summary)}
+                <br />
+                Rating: 
+                <b>{series.show.rating.average}</b>
+                <br />
+            </li>)}
+        </div>
     )
 }
 export default SeriesList;
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,34 +1,27 @@
 import React, {Component} from 'react';
 import SeriesList from '../../components/SeriesList'
-import ReactHtmlParser from 'react-html-parser'; 
+
 
 class Series extends Component{
     
         state = {
-            image: [],
+
             series: [],
-            rating: []
+        
         }
         componentDidMount(){
             fetch('http://api.tvmaze.com/search/shows?q=steven-universe')
                 .then((response) => {
                     return response.json();
                 }).then((data) => {
-                    this.setState({ image: data[0].show.image.medium })
-                    this.setState({ series: data[0].show.summary })
-                    this.setState({ rating: data[0].show.rating.average })
+                    this.setState({ series: data})
                 })
         }
     render() {
         return (
             <div>   
                 <br/>
-                <img src={this.state.image}/>
-                <br/>
-                Summary: {ReactHtmlParser(this.state.series)}
-                Rating: <b>{ReactHtmlParser(this.state.rating)}</b>
-
-                <SeriesList />
+                <SeriesList list = {this.state.series}/>
             </div>
         )
     };
