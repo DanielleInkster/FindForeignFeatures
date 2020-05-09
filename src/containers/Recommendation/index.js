15e24e16ce611937b0e60c49392b68da6c58ea6f
DanielleInkster <danielle.inkster@gmail.com>
2020-05-08 22:00:55 -0600

    Can fetch keywords for Recommendation

--- src/containers/Recommendation/index.js
+++ src/containers/Recommendation/index.js
@@ -5,18 +5,29 @@ class Recommmendation extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { genres: [],
-                       rating: '',
+        this.state = { genres: [this.props.data.genre_ids],
+                       rating: this.props.data.vote_average,
                        keywords:[],
                          }
 
     }
 
+    componentDidUpdate() {
+        fetch(`https://api.themoviedb.org/3/tv/${this.props.data.id}/keywords?api_key=${API_KEY}`) 
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                    console.log(data.results)
+                    this.setState({ keywords: data.results.slice(0, 3) })
+                })
+            }
+    
+
 
     render() {
         return (
             <div>
-                {this.props.data.name}
+               Hello
             </div>
         )
     };
