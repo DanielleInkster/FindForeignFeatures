7a6c9771e295a3bdc7e876ac6eb37873a87518fa
DanielleInkster <danielle.inkster@gmail.com>
2020-05-17 23:03:05 -0600

    Fix for no release date

---
+++ src/components/FindYear/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HtmlParser from '../HtmlParser';
+
+const FindYear =(props) =>{
+    let year =''
+    if (props.movie.hasOwnProperty("release_date")){ 
+        year = props.movie.release_date.slice(0, 4)
+    } else {
+        year = "[Unknown]"
+    }
+    return(
+    <div>
+    {<HtmlParser text ={`Year of Release: <b>${year}</b>`}/>} 
+    </div>
+    )
+}
+
+export default FindYear
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -3,17 +3,19 @@ import HtmlParser from '../HtmlParser';
 import ISO6391 from 'iso-639-1';
 import ImageUrl from '../ImageUrl';
 import NonEnglishName from '../NonEnglishName';
+import FindYear from '../FindYear'
+
 
 const ListItem =(props)=>{
     return(
     <div>
         <ImageUrl movie={props.movie} />
         <h2><b>{props.movie.original_title}</b></h2>
-        {<HtmlParser text={`Year of Release: <b>${props.movie.release_date.slice(0,4)}</b>`} />}
-        <h4>{<NonEnglishName movie={props.movie} />}</h4>
-        {<HtmlParser text={props.movie.overview} /> }
+        <FindYear movie ={props.movie} />
+        <h4><NonEnglishName movie={props.movie} /></h4>
+        <HtmlParser text={props.movie.overview} /> 
           <br /> <br />
-        {<HtmlParser text={`Original language: <b>${ISO6391.getName(props.movie.original_language)}</b>`} /> }
+        <HtmlParser text={`Original language: <b>${ISO6391.getName(props.movie.original_language)}</b>`} />
           <br /> <br />
     </div>
     )
