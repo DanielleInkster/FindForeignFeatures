cb5c59ae1b3286019fdbc08e58dddef13fbee1af
DanielleInkster <danielle.inkster@gmail.com>
2020-05-18 01:14:21 -0600

    Sorts results by most returns

--- src/containers/Recommendations/index.js
+++ src/containers/Recommendations/index.js
@@ -8,29 +8,30 @@ class Recommendations extends Component {
         this.state = {
             rawKeywordRecommendations:[],
             comparedRecommendations: [],
+            sorted: false,
         }
     }
 
     rawKeywordHandler = (results) => {
         this.setState({ rawKeywordRecommendations: results });
-        // console.log(results)
     }
 
     comparedHandler = (results) => {
         this.setState({ comparedRecommendations: results });
-        console.log(results)
     }
 
-
-
+    sortResults=(arr)=>{
+        console.log(arr.sort((a, b) => (b.returns > a.returns) ? 1 : ((a.returns > b.returns)? -1 : 0)))
+         this.setState({sorted: true})
+    }
 
     render(){
         return(
             <div>
                 <Keywords movie = {this.props.movie} isFetching = {this.props.isFetching} rawKeywordHandler ={this.rawKeywordHandler}/>
                 <Genres movie={this.props.movie} isFetching={this.props.isFetching} comparedHandler={this.comparedHandler} keywordRecs = {this.state.rawKeywordRecommendations}/>
-                {/* {this.state.rawGenreRecommendations.length !== 0 && this.state.rawKeywordRecommendations.length !== 0 && this.state.conCatRecommendations.length=== 0
-                    && this.countResults(this.state.rawGenreRecommendations, this.state.rawKeywordRecommendations)} */}
+                {this.state.comparedRecommendations.length !== 0 && this.state.sorted === false && 
+                     this.sortResults(this.state.comparedRecommendations)}
             </div >  
         )
     }
