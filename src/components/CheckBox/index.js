d82bd3d5df342123f716b76e3e824b42b3c5e462
DanielleInkster <danielle.inkster@gmail.com>
2020-05-21 17:18:55 -0600

    Capitalize checkbox entry

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './index.css'
 
 export const CheckBox = props => {
     return (
         <li>
-            <input key={props.id} onChange={props.handleCheckChildElement} type="checkbox" checked={props.isChecked} value={props.value} /> {props.value}
+            <input key={props.id} onChange={props.handleCheckChildElement} type="checkbox" 
+                checked={props.isChecked} value={props.value} /> <span class='CheckBox'> {props.value}</span>
         </li>
     )
 }
