9cea580b1112b42af3ed5f7fad686c47e396322c
DanielleInkster <danielle.inkster@gmail.com>
2020-05-26 00:59:31 -0600

    Added border to Loading gif

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import loaderSrc from '../../Images/loading.gif';
+import './Loading.css'
 
 const Loading =()=>(
     <div>
-        <img src={loaderSrc} alt="Loading" />
+        <img src={loaderSrc} alt="Loading" id="Loading"/>
     </div>   
 )
 export default Loading
