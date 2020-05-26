c8f8dd279f823b2920bdbf4de8e51370fdae8328
DanielleInkster <danielle.inkster@gmail.com>
2020-05-25 23:27:24 -0600

    Fix for 3+ line titles

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Title =(props)=>{
     let searchTerm = props.type === 'tv' ? "original_name" : "original_title"
-    return props.item[searchTerm] 
+    return props.item[searchTerm].length < 50 ? props.item[searchTerm] : props.item[searchTerm].slice(0,50)+"..."
 }
 
 export default Title
