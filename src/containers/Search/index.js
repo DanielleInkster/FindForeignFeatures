6e2e875fc901ef210d6cdadf80dc60a66986173c
DanielleInkster <danielle.inkster@gmail.com>
2020-06-05 04:36:59 -0600

    SelectKeywords returns keywords to Search

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -5,14 +5,24 @@ class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            keywords:[]
         
         }
     }
+
+    componentDidMount(){
+        if (this.state.keywords.length === 0 && this.props.location.keywords !== undefined) { this.setState({ keywords: this.props.location.keywords })}
+    }
+
     render() {
         return (
             <div>
-            {console.log(this.props)}
+                {console.log(this.state.keywords)}
+            {this.state.keywords.length ===0 &&
             <Keywords item={this.props} />
+            }
+            {this.state.keywords.length !== 0 &&
+            "Hello, I've got keywords!"}
             </div>
         )
     }
