6f30963b5009f39bd9df56ed3475d603fe307243
DanielleInkster <danielle.inkster@gmail.com>
2020-06-09 03:51:59 -0600

    Add outline to title on hover

--- src/components/ChooseMedia/index.js
+++ src/components/ChooseMedia/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Button from '../Button';
 
-
 const ChooseMedia = () => {
  
         return (
             <div>
                 <Link to={{pathname: '/movie'}}>
-                <Button value="Film" />
+                <Button value="Film"/>
                 </Link>
 
                 <Link to={{pathname: '/tv'}}>
-                <Button value="TV Series" />
+                    <Button value="TV Series"/>
                 </Link>
             </div>
         );
