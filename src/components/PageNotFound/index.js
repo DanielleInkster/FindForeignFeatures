92e3fba22a26538564c17b042b7b1782e27dbea2
DanielleInkster <danielle.inkster@gmail.com>
2020-06-14 16:32:21 -0600

    tweaked styles

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import Button from '../Button'
 import NotFound from '../../Images/404.gif';
 import './PageNotFound.css'
 
 const PageNotFound = () => (
     <div>
         <img src={NotFound} alt="Not Found" id="NotFound" />
+        <br/>
+        <Link to={{ pathname: '/' }}>
+            <Button value="Start a New Search" />
+        </Link>
     </div>
 )
 export default PageNotFound
