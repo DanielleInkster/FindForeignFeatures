7be6a50d591ea4a4e665bf500fe2d0692629efbf
DanielleInkster <danielle.inkster@gmail.com>
2020-06-18 20:43:37 -0600

    Added collapsible elements to FAQ

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import './FAQ.css'
 
 const FAQ = () => {
     return (
         <div>
-            I MUST HAVE CALLED A THOUSAND TIMES
-        </div>
+            <div class="wrap-collabsible">
+                <input id="collapsible" class="toggle" type="checkbox"/>
+                    <label for="collapsible" class="lbl-toggle" id="label">More Info</label>
+                    <div class="collapsible-content">
+                        <div class="content-inner">
+                            <p>
+                                QUnit is by calling one of the object that are embedded in JavaScript, and faster JavaScript program could also used with
+                                its elegant, well documented, and functional programming using JS, HTML pages Modernizr is a popular browsers without
+                                plug-ins. Test-Driven Development.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+            <div class="wrap-collabsible">
+                <input id="collapsible2" class="toggle2" type="checkbox" />
+                <label for="collapsible2" class="lbl-toggle2" id="label2">More Info</label>
+                <div class="collapsible-content2">
+                    <div class="content-inner2">
+                        <p>
+                            QUnit is by calling one of the object that are embedded in JavaScript, and faster JavaScript program could also used with
+                            its elegant, well documented, and functional programming using JS, HTML pages Modernizr is a popular browsers without
+                            plug-ins. Test-Driven Development.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            </div>
+    
+     
+     
     )
 
 }
