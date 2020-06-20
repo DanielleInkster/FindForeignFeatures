64c0abe88845e100594562e0fedffebdf772e23c
DanielleInkster <danielle.inkster@gmail.com>
2020-06-20 03:21:55 -0600

    Second Attempt to deploy with Express

---
+++ server/build/precache-manifest.d481bf76c6111a5abbceace337ec2492.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152875f60fcde766db0002b612c18c79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5fc92c79f14710e2bc",
+    "url": "/static/css/main.caae1bc9.chunk.css"
+  },
+  {
+    "revision": "e1186db5a48a58a77655",
+    "url": "/static/js/2.46b041f6.chunk.js"
+  },
+  {
+    "revision": "a636ddd461bd633ab63e86040460d655",
+    "url": "/static/js/2.46b041f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5fc92c79f14710e2bc",
+    "url": "/static/js/main.a1d278b6.chunk.js"
+  },
+  {
+    "revision": "fa9de4a05754bf1aed85",
+    "url": "/static/js/runtime-main.3ec072f9.js"
+  },
+  {
+    "revision": "0b56b5409427dbafd10ed899fc49e6e7",
+    "url": "/static/media/404.0b56b540.gif"
+  },
+  {
+    "revision": "a78d55b6accf1a42c8f7459bfea64e63",
+    "url": "/static/media/NoImage.a78d55b6.png"
+  },
+  {
+    "revision": "84abea451db783c01e0056252f8045f6",
+    "url": "/static/media/camera.84abea45.png"
+  },
+  {
+    "revision": "88d4dd718e965cb300e28b27ea36a242",
+    "url": "/static/media/loading.88d4dd71.gif"
+  }
+]);
