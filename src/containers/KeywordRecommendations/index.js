563dfbdeead9660f284d8b5f397feae6e7c3746c
DanielleInkster <danielle.inkster@gmail.com>
2020-06-22 19:24:47 -0600

    Second try api"

--- src/containers/KeywordRecommendations/index.js
+++ src/containers/KeywordRecommendations/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-const API_KEY = `${process.env.REACT_APP_DB_API_KEY}`
+const API_KEY = `${process.env.API_KEY}`
 
 class KeywordRecommendations extends Component {
     constructor(props) {
