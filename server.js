755e5928d1c7474fa27f7caa102cdad067b65716
DanielleInkster <danielle.inkster@gmail.com>
2020-06-25 17:58:53 -0600

    Installed node-fetch

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ app.use(express.static(__dirname));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
-
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+
 app.listen(port);
