a41666dd3052e90fed0176dd419487e9cb7725ff
DanielleInkster <danielle.inkster@gmail.com>
2020-07-02 01:42:14 -0700

    moved analytics to index

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
 import Routes from './Routes';
 import { BrowserRouter as Router } from "react-router-dom"
 
 import ScrollButton from './Assets/ScrollButton';
 
 import '../stylesheets/App.css'
-
-const trackingId = "UA-171500069-1"; 
-ReactGA.initialize(trackingId);
-
-
-
-const history = createBrowserHistory();
-history.listen(location => {
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
-
 
 
 
