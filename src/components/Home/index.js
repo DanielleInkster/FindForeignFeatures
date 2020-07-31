53cd5f7e8ef0e45707e1b1afcc5b79fa01ca4614
DanielleInkster <danielle.inkster@gmail.com>
2020-07-30 21:38:08 -0600

    Got rid of underline on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,7 @@ import '../../stylesheets/Home/Home.css'
 
 const Home=()=> {
 
-    let input = "Welcome to <u><span id='SiteName'>Find Foreign Features</span></u>, the site that recommends foreign " +
+    let input = "Welcome to <span id='SiteName'>Find Foreign Features</span>, the site that recommends foreign " +
       "films and TV series based on the English ones you already love."
     let input2 = "<div id='text'>What are you looking for today?</text>"
 
