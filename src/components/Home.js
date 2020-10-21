db4630873040c73e9de4f67a60823675c047d750
DanielleInkster <danielle.inkster@gmail.com>
2020-10-21 14:48:25 -0400

    Extracted CM,Hme,T

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ChooseMedia from '../ChooseMedia';
-import Message from '../Assets/Message';
-import '../../stylesheets/Home/Home.css'
+import ChooseMedia from './ChooseMedia';
+import Message from './Assets/Message';
+import './../stylesheets/Home/Home.css'
 
 
 const Home=()=> {
