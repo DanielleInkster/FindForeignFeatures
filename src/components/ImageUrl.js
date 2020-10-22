8a40457de18d15018365524f7aa42bb3ed51d3b8
DanielleInkster <danielle.inkster@gmail.com>
2020-10-21 22:32:14 -0400

    most components

--- src/components/ImageUrl.js
+++ src/components/ImageUrl.js
@@ -2,17 +2,13 @@ import React from "react";
 import NoImage from "./../Images/NoImage.png";
 import "./../stylesheets/ImageUrl/ImageUrl.css";
 
-const ImageUrl = (props) => {
+const ImageUrl = ({ item }) => {
   let picSource =
-    props.item.poster_path != null
-      ? `https://image.tmdb.org/t/p/w500/${props.item.poster_path}`
+    item.poster_path != null
+      ? `https://image.tmdb.org/t/p/w500/${item.poster_path}`
       : NoImage;
   return (
-    <img
-      src={picSource}
-      alt={props.item.original_name + " poster."}
-      id="poster"
-    />
+    <img src={picSource} alt={item.original_name + " poster."} id="poster" />
   );
 };
 export default ImageUrl;
